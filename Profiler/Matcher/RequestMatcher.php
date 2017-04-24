66164ce0f302c71629ba020ff544cfd58297fdf2
Mathieu Delisle <mdelisle@webonaute.ca>
2017-04-24 18:00:06 -0400

    add code to desable profiling on request containing profiler annotation with desable property to true.

--- Profiler/Matcher/RequestMatcher.php
+++ Profiler/Matcher/RequestMatcher.php
@@ -2,9 +2,11 @@
 
 namespace Webonaute\SymfonyProfilerMatcherBundle\Profiler\Matcher;
 
+use Doctrine\Common\Annotations\AnnotationReader;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestMatcherInterface;
 use Symfony\Component\Routing\Router;
+use Webonaute\SymfonyProfilerMatcherBundle\Annotation\Profiler;
 
 class RequestMatcher implements RequestMatcherInterface
 {
@@ -20,6 +22,22 @@ class RequestMatcher implements RequestMatcherInterface
 
     public function matches(Request $request)
     {
+        $reader = new AnnotationReader();
+        $route = $this->router->matchRequest($request);
+        list ($controller, $method) = explode("::", $route['_controller']);
+        $methodReflection = new \ReflectionMethod($controller, $method);
+
+        /** @var Profiler $classAnnotation */
+        $classAnnotation = $reader->getMethodAnnotation(
+            $methodReflection, 'Webonaute\SymfonyProfilerMatcherBundle\Annotation\Profiler'
+        );
+
+        if ($classAnnotation !== null and $classAnnotation->desable == true){
+            //desable profiling on this request.
+            return false;
+        }
+
+        //keep profiling on.
         return true;
     }
 }
